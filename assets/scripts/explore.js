a900b3f8d2045daf2dfef15f8293567ba33450dd
edeolive <edeolive@ucsd.edu>
2021-10-24 00:29:09 -0700

    Added explore functionality

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,37 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const speechSynthesis = window.speechSynthesis;
+  const voiceSelector = document.getElementById('voice-select');
+  const playButton = document.querySelector('button');
+  const textToSpeak = document.getElementById('text-to-speak');
+  const faceImg = document.querySelector('img');
+  let voices;
+
+  /* voice population is done with a timeout because of chrome getVoices() delay */
+  setTimeout(() => {
+    voices = speechSynthesis.getVoices();
+    for (const voice of voices) {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.name;
+      voiceSelector.appendChild(option);
+    }
+  }, 50);
+
+  playButton.addEventListener('click', (e) => {
+    const utterance = new SpeechSynthesisUtterance(textToSpeak.value)
+    utterance.voice = voices.find((voice) => voice.name === voiceSelector.options[voiceSelector.selectedIndex].value);
+    speechSynthesis.speak(utterance);
+  })
+
+  function moveMouth() {
+    if (speechSynthesis.speaking) {
+      faceImg.src = `assets/images/smiling-open.png`;
+    } else {
+      faceImg.src = `assets/images/smiling.png`;
+    }
+  }
+
+  setInterval(moveMouth, 200);
 }
